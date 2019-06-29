7c53b9a3eea715f30c6c6a94532e04c15f4c4d99
Carlos Vaquedano <chvb2002@hotmail.com>
2019-06-29 09:49:38 -0600

    skeleton with animation

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Componente } from '../interface/interfaces';
+import { delay } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -21,6 +22,8 @@ export class DataService {
   }
 
   getHeroes(){
-    return this.http.get('/assets/data/superheroes.json')
+    return this.http.get('/assets/data/superheroes.json').pipe(
+      delay(2000)
+    );
   }
 }
