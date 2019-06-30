25ef831a8b36558a9910339c38475a3e8fbb0a15
Carlos Vaquedano <chvb2002@hotmail.com>
2019-06-30 11:22:18 -0600

    toast

--- src/app/pages/list/list.page.ts
+++ src/app/pages/list/list.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { DataService } from 'src/app/services/data.service';
 import { Observable } from 'rxjs';
-import { IonList } from '@ionic/angular';
+import { IonList, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-list',
@@ -12,22 +12,31 @@ export class ListPage implements OnInit {
   usuarios: Observable<any>;
   @ViewChild('lista') lista:IonList;
 
-  constructor(private dataService:DataService) { }
+  constructor(private toastCtrl:ToastController,
+     private dataService:DataService) { }
 
   ngOnInit() {
     this.usuarios=this.dataService.getUsers();
   }
 
+  async presentToast(message:string){
+    const toast = await this.toastCtrl.create({
+      message,
+      duration: 2000
+    });
+    toast.present();
+  }
+
   favorite(user){
-    console.log('favorite',user);
+    this.presentToast('Guardo en favoritos')
     this.lista.closeSlidingItems();
   }
   share(user){
-    console.log('share',user);
+    this.presentToast('Compartido!')
     this.lista.closeSlidingItems();
   }
   borrar(user){
-    console.log('borrar',user);
+    this.presentToast('Borrado :(')
     this.lista.closeSlidingItems();
   }
 
